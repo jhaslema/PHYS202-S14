faf82c46680259a428f99d827553eb0aeb9a5823
Jake Haslemann <jhaslema@calpoly.edu>
2014-04-03 15:01:59 -0700

    track Config script and pledge

---
+++ ConfigureGit.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+git config --global user.name "Jake Haslemann"
+git config --global user.email "jhaslema@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
+
